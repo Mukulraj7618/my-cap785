e6719c5516f976cae3beed1656c6ed6289aebf68
Mukul Raj <895839mukulgoodboy@gmail.com>
2025-02-04 21:38:49 +0530

    Your commit message

--- http.js
+++ http.js
@@ -1,6 +1,21 @@
 var express = require("express");
 var app = express();
 
-// Run static server
+
+var PORT = process.env.PORT || 3000;
+
+
 app.use(express.static(__dirname));
-app.listen(8080);
+
+var server = app.listen(PORT, function () {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+
+server.on("error", function (err) {
+    if (err.code === "EADDRINUSE") {
+        console.error(`Port ${PORT} is already in use. Try a different port.`);
+    } else {
+        console.error("Server Error:", err);
+    }
+});
